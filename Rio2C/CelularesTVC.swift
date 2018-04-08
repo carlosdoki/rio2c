9cded959a94958d51dd087e267010c34be673a21
Carlos Doki <carlosdoki@gmail.com>
2018-04-08 00:00:00 -0300

    first commit

---
+++ Rio2C/CelularesTVC.swift
@@ -0,0 +1,24 @@
+//
+//  CelularesTVC.swift
+//  Rio2C
+//
+//  Created by Carlos Doki on 07/04/18.
+//  Copyright © 2018 Carlos Doki. All rights reserved.
+//
+
+import UIKit
+
+class CelularesTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Rio2C/Util/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Rio2C
+//
+//  Created by Carlos Doki on 07/04/18.
+//  Copyright © 2018 Carlos Doki. All rights reserved.
+//
+
+import Foundation
